2e4b004e7344cfc703d77fea932dfdc8cff42059
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-03-05 18:58:42 +0700

    W03

---
+++ W03/user-service/src/main/java/iuh/fit/se/userservice/auths/UserPrincipal.java
@@ -0,0 +1,37 @@
+package iuh.fit.se.userservice.auths;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Getter
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
